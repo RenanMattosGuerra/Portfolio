deb40986a266939df673b858ed1445729e2b1f3b
RenanGuerra <rguerra1998@gmail.com>
2025-02-10 16:48:34 -0300

    home + blog new

---
+++ Blog/mainblog.js
@@ -0,0 +1,13 @@
+/*
+const text = document.querySelector(".description")
+const imgs = document.querySelector(".animals")
+const textValue = text.Value
+
+imgs.addEventListener("mouseover", function(){
+    if(textValue.length>0){
+        text.style.display="none"
+    } else{
+        text.style.display="flex"
+    }    
+})
+*/
