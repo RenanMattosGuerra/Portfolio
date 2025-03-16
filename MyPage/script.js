ffb56ddfb4937ecba7c66b3d99b29de4507ed99e
RenanGuerra <rguerra1998@gmail.com>
2025-03-16 13:59:36 -0300

    MyPage New

---
+++ MyPage/script.js
@@ -0,0 +1,41 @@
+const openMenu = document.getElementById("bars");
+const closeMenu = document.getElementById("xicon");
+const menu = document.getElementById("menu");
+const links = document.querySelectorAll("#a1, #a2, #a3, #a4");
+
+openMenu.addEventListener("click", () => {
+  menu.style.display = "flex";
+  openMenu.style.display = "none";
+  closeMenu.style.display = "block";
+});
+
+closeMenu.addEventListener("click", () => {
+  menu.style.display = "none";
+  openMenu.style.display = "block";
+  closeMenu.style.display = "none";
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    menu.style.display = "none";
+    openMenu.style.display = "block";
+    closeMenu.style.display = "none";
+  });
+});
+
+const photo = document.getElementById("foto");
+
+photo.addEventListener("mouseover", () => {
+  photo.classList.toggle("animefoto");
+});
+
+const page = document.getElementById("page");
+const lightdark = document.getElementById("modebox");
+const titles = document.querySelectorAll("h2,h3");
+
+lightdark.addEventListener("change", () => {
+  page.classList.toggle("lightmode");
+  titles.forEach((title) => {
+    title.classList.toggle("lightmode");
+  });
+});
--- Noticias/newsscript.js
+++
@@ -1,7 +0,0 @@
-function ModoTrocar(radio) {
-    if (radio.value === 'mescuro') {
-        document.body.classList.add('modo-escuro'); // Adiciona o modo escuro
-    } else {
-        document.body.classList.remove('modo-escuro'); // Remove o modo escuro
-    }
-}
