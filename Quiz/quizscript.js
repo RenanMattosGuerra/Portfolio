e451eb75c9e99321ef157733abf686d75b60e7ae
RenanGuerra <rguerra1998@gmail.com>
2025-04-04 01:44:52 -0300

    quiz question

--- Quiz/quizscript.js
+++ Quiz/quizscript.js
@@ -8,6 +8,7 @@ const one = document.getElementById("one");
 const two = document.getElementById("two");
 const three = document.getElementById("three");
 const four = document.getElementById("four");
+const five = document.getElementById("five");
 const end = document.getElementById("fim");
 const result = document.getElementById("resultado");
 
@@ -34,16 +35,19 @@ right.forEach((element) => {
     element.style.backgroundColor = "green";
     rightCount++;
     if (rightCount == 1) {
-      result.innerHTML = "Você acertou 1/4";
+      result.innerHTML = "Você acertou 1/5";
     }
     if (rightCount == 2) {
-      result.innerHTML = "Você acertou 2/4";
+      result.innerHTML = "Você acertou 2/5";
     }
     if (rightCount == 3) {
-      result.innerHTML = "Você acertou 3/4";
+      result.innerHTML = "Você acertou 3/5";
     }
     if (rightCount == 4) {
-      result.innerHTML = "Você acertou 4/4";
+      result.innerHTML = "Você acertou 4/5";
+    }
+    if (rightCount == 5) {
+      result.innerHTML = "Você acertou 5/5";
     }
   });
 });
@@ -67,12 +71,19 @@ button.addEventListener("click", () => {
     two.style.display = "none";
     three.style.display = "none";
     four.style.display = "flex";
-    button.innerHTML = "Resultado";
   }
   if (buttonClick == 4) {
     two.style.display = "none";
+    three.style.display = "none";
+    four.style.display = "none";
+    five.style.display = "flex";
+    button.innerHTML = "Resultado";
+  }
+  if (buttonClick == 5) {
+    two.style.display = "none";
     three.scroll.display = "none";
     four.style.display = "none";
+    five.style.display = "none";
     button.style.visibility = "hidden";
     end.style.display = "block";
     result.style.display = "block";
