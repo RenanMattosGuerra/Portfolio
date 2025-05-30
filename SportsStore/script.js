4c65a23c40ccce054d38dae32de7773bcdc154fb
RenanGuerra <rguerra1998@gmail.com>
2025-05-29 22:09:10 -0300

    sports store

---
+++ SportsStore/script.js
@@ -0,0 +1,26 @@
+//Abrir e fechar carrinho
+
+const openCart = document.getElementById("openCart");
+const cartMenu = document.getElementById("cartMenu");
+
+openCart.addEventListener("click", () => {
+  cartMenu.classList.toggle("open");
+});
+
+//Abrir e fechar menu de navegação
+
+const navOpen = document.getElementById("navOpen");
+const navMenu = document.getElementById("navMenu");
+const navBar = document.getElementById("nav");
+
+navBar.addEventListener("click", () => {
+  navMenu.classList.toggle("displayMenu");
+});
+
+navBar.addEventListener("mouseover", () => {
+  navOpen.classList.add("color");
+});
+
+navBar.addEventListener("mouseout", () => {
+  navOpen.classList.remove("color");
+});
