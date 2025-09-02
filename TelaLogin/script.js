097c863877801f4e2ba807cf221a1f1a355bb2a8
RenanGuerra <rguerra1998@gmail.com>
2025-09-01 21:57:24 -0300

    fix show/hide password

--- TelaLogin/script.js
+++ TelaLogin/script.js
@@ -1,17 +1,20 @@
+//Tela de carregamento / Loading Screen
+
 window.addEventListener("load", () => {
   const preloader = document.getElementById("preloader");
   const page = document.getElementById("page");
 
   preloader.style.display = "none";
   page.style.display = "flex";
-
-  function ShowHide() {
-    let senha = document.getElementById("pword");
-
-    if (senha.type === "password") {
-      senha.setAttribute("type", "text");
-    } else {
-      senha.setAttribute("type", "password");
-    }
-  }
 });
+
+// Mostrar/Ocultar Senha - Show/Hide Password
+function ShowHide() {
+  let senha = document.getElementById("pword");
+
+  if (senha.type === "password") {
+    senha.setAttribute("type", "text");
+  } else {
+    senha.setAttribute("type", "password");
+  }
+}
